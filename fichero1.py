5c3ff2075eca85d8cf32aba83314f9cfa8b46d5e
jesusgutierrezburgos <162326238+jesusgutierrezburgos@users.noreply.github.com>
2024-03-12 14:33:19 +0100

    Update fichero1.py

--- fichero1.py
+++ fichero1.py
@@ -1,4 +1,4 @@
 print("Fichero 1")
 print("Nueva línea fichero1 desde GitHub")
 print("Nueva línea fichero1 desde VSC")
-prnt("Nueva línea fichero1 desde diseñador1 en GitHub")
+print("Nueva línea fichero1 desde diseñador1 en GitHub")
